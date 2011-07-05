c2943d6828443c7869ce7c98bfbb8c921ace31ca
jesperrasmussen78 <jesperrasmussen78@gmail.com>
2011-07-05 20:46:53 +0000

    Got a backend working with REST now

---
+++ service-api/trunk/src/com/jbr/dailyfinance/api/service/TicketLineServices.java
@@ -0,0 +1,11 @@
+package com.jbr.dailyfinance.api.service;
+
+import com.jbr.dailyfinance.api.repository.server.TicketLineSecurable;
+
+/**
+ *
+ * @author jbr
+ */
+public interface TicketLineServices extends BasicOperations<TicketLineSecurable> {
+    
+}
