fda2b0de8a0afe335146b7cccd8e1c2bd4ed383c
jesperrasmussen78 <jesperrasmussen78@gmail.com>
2011-07-08 21:00:36 +0000

    Lib for GWT json things

---
+++ gwt-json-lib/trunk/src/com/jbr/gwt/json/JsonUtils.java
@@ -0,0 +1,77 @@
+package com.jbr.gwt.json;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jbr
+ */
+public class  JsonUtils {
+    public final static DateTimeFormat jsonFormat = DateTimeFormat.getFormat("yyyy-MM-dd");
+
+    /**
+    * Convert the string of JSON into JavaScript object.
+    */
+    public static <T extends JavaScriptObject> JsArray<T>
+            asArrayOf(Class<T> c, String jsonObjectName, String jsonString) throws RequestException {
+        JSONValue jv = JSONParser.parseStrict(jsonString);
+        JSONObject o = jv.isObject();
+        if (o == null || !o.containsKey(jsonObjectName))
+            throw new RequestException("No " + jsonObjectName + " object found in return string from server");
+        else {
+            final JSONValue get = o.get(jsonObjectName);
+            return  (JsArray) get.isArray().getJavaScriptObject();
+        }
+    }
+
+    public static <T extends JavaScriptObject> List<T>
+            asListOf(Class<T> c, String jsonObjectName, String jsonString) throws RequestException {
+        JSONValue jv = JSONParser.parseStrict(jsonString);
+        JSONObject o = jv.isObject();
+        if (o == null || !o.containsKey(jsonObjectName))
+            throw new RequestException("No " + jsonObjectName + " object found in return string from server");
+
+        final List<T> list = new ArrayList<T>();
+        final JSONValue get = o.get(jsonObjectName);
+        if (get.isArray() == null)
+            list.add((T) get.isObject().getJavaScriptObject());
+        else {
+            final JsArray jsArray = (JsArray)get.isArray().getJavaScriptObject();
+            for (int i= 0; i < jsArray.length(); i++)
+                list.add((T) jsArray.get(i));
+        }
+        return list;
+    }
+
+    public static <T extends JavaScriptObject> List<T> asListOf(JsArray<T> jsArray) {
+        List<T> ret = new ArrayList<T>(jsArray.length());
+        for (int i= 0; i < jsArray.length(); i++)
+            ret.add(jsArray.get(i));
+        return ret;
+    }
+
+    public interface ListCallback<T> {
+        void onResponseOk(List<T> list);
+    }
+
+    public interface RemoveCallback {
+        void onResponseOk();
+    }
+
+    public interface StringCallback {
+        void onResponseOk(String string);
+    }
+
+    public interface ElementCallback<T extends JavaScriptObject, B> {
+        void onResponseOk(T element, B backref);
+    }
+
+}
