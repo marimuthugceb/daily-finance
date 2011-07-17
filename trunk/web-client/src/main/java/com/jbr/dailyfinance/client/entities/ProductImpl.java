fc594f99e73230eb2561c3e7da09201d0b08d7ed
jesperrasmussen78@gmail.com <jesperrasmussen78@gmail.com@c14ff3a4-ec32-1362-86ab-fea42e5e8c97>
2011-07-17 14:11:17 +0000

    Entity impl for gwt

--- trunk/web-client/src/main/java/com/jbr/dailyfinance/client/entities/ProductImpl.java
+++ trunk/web-client/src/main/java/com/jbr/dailyfinance/client/entities/ProductImpl.java
@@ -12,17 +12,28 @@ public class ProductImpl extends JavaScriptObject implements Product, JsonEntity
 
     protected ProductImpl() {}
 
+    @Override
     public final native Long getId() /*-{ return this.id; }-*/;
+    @Override
     public final native String getName() /*-{ return this.name; }-*/;
+    @Override
     public final native void setName(String newname) /*-{ this.name = newname; }-*/;
     public final native void setId(Long newid) /*-{ this.id = newid; }-*/;
+    @Override
     public final native Long getCategoryId() /*-{ return this.categoryId; }-*/;
+    @Override
     public final native void setCategoryId(Long categoryid) /*-{ this.categoryId = categoryid; }-*/;
+    @Override
     public final native String getEan() /*-{ return this.ean; }-*/;
+    @Override
     public final native void setEan(String ean) /*-{ this.ean = ean; }-*/;
+    @Override
     public final native String getManufacturer() /*-{ return this.manu; }-*/;
+    @Override
     public final native void setManufacturer(String manu) /*-{ this.manu = manu; }-*/;
+    @Override
     public final native Double getPrice() /*-{ return this.price; }-*/;
+    @Override
     public final native void setPrice(Double price) /*-{ this.price = price; }-*/;
 
     @Override
---
+++ trunk/web-client/src/main/java/com/jbr/dailyfinance/client/entities/TicketImpl.java
@@ -0,0 +1,61 @@
+package com.jbr.dailyfinance.client.entities;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.json.client.JSONObject;
+import com.jbr.dailyfinance.api.repository.client.Ticket;
+import com.jbr.gwt.json.client.JsonUtils;
+import java.util.Date;
+
+/**
+ *
+ * @author jbr
+ */
+public class TicketImpl extends JavaScriptObject implements Ticket, JsonEntity<TicketImpl> {
+
+    protected TicketImpl() {}
+
+    @Override
+    public final native Long getId() /*-{ return this.id; }-*/;
+    public final native void setId(Long newid) /*-{ this.id = newid; }-*/;
+    public final native void setName(String newname) /*-{ this.name = newname; }-*/;
+    @Override
+    public final native Long getStoreId() /*-{ return this.storeid; }-*/;
+    @Override
+    public final native void setStoreId(Long mStoreId) /*-{ this.storeid = mStoreId; }-*/;
+    public final native String getTicketDateRaw() /*-{ return this.ticketdate; }-*/ ;
+    public final native void setTicketDateRaw(String mTicketDate) /*-{ this.ticketdate = mTicketDate; }-*/;
+
+    @Override
+    public final Date getTicketDate() {
+        if (getTicketDateRaw() == null)
+            return null;
+        return JsonUtils.jsonFormat.parse(getTicketDateRaw().substring(0, 10));
+    }
+
+    @Override
+    public void setTicketDate(Date mTicketDate) {
+        setTicketDateRaw(JsonUtils.jsonFormat.format(mTicketDate));
+    }
+
+    public final String getDateAsString() {
+        final DateTimeFormat userFormat = DateTimeFormat.getFormat(
+                DateTimeFormat.PredefinedFormat.DATE_FULL);
+        return userFormat.format(getTicketDate());
+
+    }
+
+    @Override
+    public final TicketImpl toNewJsonEntity() {
+        return this;
+    }
+
+    @Override
+    public final String toJson() {
+        return new JSONObject(this).toString();
+    }
+
+
+
+
+}
